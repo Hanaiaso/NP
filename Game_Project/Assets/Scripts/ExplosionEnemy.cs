a96d1ed396f322ead1ab315ffa8abfdeded6ef05
linhmaideptrai <duylinh2992004@gmail.com>
2025-10-07 03:06:06 +0700

    code xong quai no

---
+++ Game_Project/Assets/Scripts/ExplosionEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionEnemy : Enemy
+{
+    [SerializeField] private GameObject explosionPrefabs; // tạo game object
+
+    private void CreateExplosion()
+    {
+        if (explosionPrefabs != null) 
+        {
+        Instantiate(explosionPrefabs, transform.position, Quaternion.identity);  // instant một vụ nổ ở enemy
+        }
+    }
+
+    protected override void Die()
+    {
+        CreateExplosion();
+        base.Die();
+    }
+    private void OnTriggerEnter2D(Collider2D collision) // cha chạm với player để tạo vụ nổ
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            CreateExplosion();
+        }
+    }
+
+}
