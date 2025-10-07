fa12b9f9ddb16597c5bd59185b8272a2c0cc5cda
Khai Tong <ttk312004@gmail.com>
2025-10-07 10:21:11 +0700

    fix enemy can passing through objects for enemy explosion

--- Game_Project/Assets/Scripts/ExplosionEnemy.cs
+++ Game_Project/Assets/Scripts/ExplosionEnemy.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class ExplosionEnemy : Enemy
 {
-    [SerializeField] private GameObject explosionPrefabs; // tạo game object
+    [SerializeField] private GameObject explosionPrefab; // hiệu ứng vụ nổ
 
     private void CreateExplosion()
     {
-        if (explosionPrefabs != null) 
+        if (explosionPrefab != null)
         {
-        Instantiate(explosionPrefabs, transform.position, Quaternion.identity);  // instant một vụ nổ ở enemy
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (player != null)
+            {
+                player.TakeDamege(enterDamage);
+            }
+            CreateExplosion(); // nổ khi chạm player
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (player != null)
+            {
+                player.TakeDamege(stayDamage);
+            }
         }
     }
 
     protected override void Die()
     {
-        CreateExplosion();
+        CreateExplosion(); // nổ khi enemy chết
         base.Die();
     }
-    private void OnTriggerEnter2D(Collider2D collision) // cha chạm với player để tạo vụ nổ
-    {
-        if (collision.CompareTag("Player")) 
-        {
-            CreateExplosion();
-        }
-    }
-
 }
